849e4bc0fc5e9d3a27a753e29243b1309b81f7df
Alex <alexandersmith223@gmail.com>
2020-02-09 12:29:04 -0800

    Get base layout established

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // TypeIt
   var type_text = [
     'Hard-working',
     'Genuine',
@@ -26,4 +27,32 @@ $(document).ready(function() {
     lifeLike: true,
     loop: true
   });
+
+  // Scrollbar for side nav
+  $('#sidebar').mCustomScrollbar({
+    theme: 'minimal'
+  });
+
+  // Menu button
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar, #content').toggleClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    $(this).toggleClass('active');
+  });
+
+  // Hide menu button until page is scrolled
+  var navbarCollapse = function() {
+    if ($(window).scrollTop() > 150) {
+      $('#sidebarCollapse').show();
+    } else {
+      $('#sidebarCollapse').hide();
+    }
+  };
+
+  // Hide menu button now if page is at the top
+  navbarCollapse();
+
+  // Hide menu button when page is scrolled
+  $(window).scroll(navbarCollapse);
 });
