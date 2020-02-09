6b6117bdbddeea1a54ff24c054387063bd3cbf4e
Alex <alexandersmith223@gmail.com>
2020-02-08 17:59:11 -0800

    Start new site
    - Middleman
    - Bootstrap
    - Typeit
    - Images
    - Favicons
    - Open Graph
    - Layout
    - Fonts

---
+++ config.rb
@@ -0,0 +1,27 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.build_before = true
+end
+
+activate :livereload
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
---
+++ source/javascripts/site.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  var type_text = [
+    'Hard-working',
+    'Genuine',
+    'Unanticipated',
+    'Smurff',
+    'Creative',
+    'Honest',
+    'Giggly',
+    'Driven',
+    'Determined',
+    'Smart',
+    'Blunt',
+    'Kind',
+    'Loyal',
+    'Stubborn',
+    'Goofy',
+    'Pessimistic',
+    'Cheesy',
+    'Annoying'
+  ];
+
+  new TypeIt('#type-text', {
+    strings: type_text,
+    breakLines: false,
+    lifeLike: true,
+    loop: true
+  });
+});
